8ca13fbc2e87cfbe2ddfe7867e84f3069fc5ebfd
udhaykantth <udhaykanthd@PRINHYLTPAP0668.local>
2015-07-22 17:35:44 +0530

    Initial Commit. Done Project setup.

---
+++ HomeKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeKitDemo
+//
+//  Created by udhaykanthd on 7/17/15.
+//  Copyright (c) 2015 udhaykanthd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeKitDemo
+//
+//  Created by udhaykanthd on 7/17/15.
+//  Copyright (c) 2015 udhaykanthd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
